9edb7a32b91fc2b56e2d65c652041730d6c0c0ca
Vlad Kornyakov <time2begin666@gmail.com>
2024-05-06 12:40:51 +0300

    HW34

---
+++ Exercises/lesson33.ts
@@ -0,0 +1,21 @@
+const dataFromControl = {
+    water: 200,
+    el: 350
+};
+
+function checkReadings_1(data: typeof dataFromControl): boolean {
+    const dataFromUser = {
+        water: 200,
+        el: 350,
+    }
+
+    if (data.el === dataFromUser.el && data.water === dataFromUser.water) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const PI = 3.14;
+
+let PIClone: typeof PI;
